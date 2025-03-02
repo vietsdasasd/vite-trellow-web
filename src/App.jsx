43b98fbe63a13da5842b1f3a299806bf158bad2b
vietsdasasd <luuviet504@gmail.com>
2025-03-02 15:26:41 +0700

    config_theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>vietsasd</div>
+      <Typography variant="body2" color="text.secondary">Test Typography</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
@@ -16,7 +18,7 @@ function App() {
       <AccessAlarmIcon/>
       <ThreeDRotation/>
     </>
-)
+  )
 }
 
 export default App
