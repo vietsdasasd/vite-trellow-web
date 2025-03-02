49633f9862b466e66a4b7971111a0f3d1177176e
vietsdasasd <luuviet504@gmail.com>
2025-03-02 16:53:29 +0700

    config_mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle/>
+      <hr/>
       <div>vietsasd</div>
       <Typography variant="body2" color="text.secondary">Test Typography</Typography>
 
