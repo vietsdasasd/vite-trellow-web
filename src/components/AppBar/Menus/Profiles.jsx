11950cccc78d739bf3f0d352d61e192ed5ad9448
vietsdasasd <luuviet504@gmail.com>
2025-03-04 19:57:56 +0700

    modified board_bar

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -32,7 +32,7 @@ function Profiles() {
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar
-            sx={{ width: 30, height: 32 }}
+            sx={{ width: 34, height: 34 }}
             alt='ảnh'/>
         </IconButton>
       </Tooltip>
--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,101 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLE = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  padding: '5px',
+  borderRadius: '4px',
+  border: 'none',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover':{
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.dark',
+    <Box px={2} sx={{
       width: '100%',
-      height: (theme) => theme.trello.boardBarHeight,
+      height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      overflowX: 'auto'
     }}>
-        Board Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLE}
+          icon={<DashboardIcon />}
+          label="Luu Huy Viet"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLE}
+          icon={<VpnLockIcon />}
+          label="public/private worksapce"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLE}
+          icon={<AddToDriveIcon />}
+          label="Add to google drive"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLE}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLE}
+          icon={<FilterListIcon/>}
+          label="Filters"
+          clickable
+        />
+
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon/>}>Invite</Button>
+
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root':{
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}>
+          <Tooltip title="luu huy viet">
+            <Avatar
+              alt="LuuHuyViet"
+              src="/static/images/avatar/1.jpg"
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
+
+
     </Box>
   )
 }
